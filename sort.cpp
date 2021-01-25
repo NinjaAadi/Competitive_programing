24107dd74f4bf2ebbc04836c8cc42c7d0dfb2e29
Aaditya <aaditya7739008423@gmail.com>
2021-01-25 21:40:11 +0530

    Class 1

---
+++ sort.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+
+#include <unordered_map>
+typedef long long ll;
+typedef unsigned long long ull;
+#define whatis(x) cout << #x << " = " << x << endl;
+#define whatis2(x, y) cout << #x << " = " << x << " and " << #y << " = " << y << endl;
+#define MOD 1000000007
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+#define testcase     \
+    long long testn; \
+    cin >> testn;    \
+    while (testn--)
+#define all(x) x.begin(), x.end(),
+#define pb push_back
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define loopn(i, n) for (ll i = 0; i <= n; i++)
+#define loop1(i, n) for (ll i = 1; i < n; i++)
+#define loop1n(i, n) for (ll i = 1; i <= n; i++)
+
+using namespace std;
+
+//Merge sort  O(nlog)
+void solve() {
+}
+int main() {
+    IOS;
+    int arr[5];  //we can store only 5 numbers
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + 5);
+    for (int i = 0; i < 5; i++) {
+        cout << arr[i];
+    }
+    return 0;
+}
---
+++ template.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>  //Includes all the files
+
+#include <unordered_map>
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define MOD 1000000007
+
+#define IOS                           \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);
+/*Used to desyncronize cin and cout with printf and scanf*/
+
+using namespace std;
+
+int main() {
+    //Time O(n)
+    int n = 10;
+    for (int i = 0; i < n; i++) {
+    }
+    //Time Complexity O(logn)
+    for (int i = 0; i < n; i++) {
+        n = n / 2;
+    }
+    //Time Complexity O(n^3)
+    for(int i = 0;i<n;i++){
+        for(int i = 0;i<n;i++){
+            for(int i = 0;i < n;i++){
+            
+            }
+        }
+    }
+
+}
