0eca9d6f75d73e5c257e97ae37b82b6067a530b0
Aaditya <aaditya7739008423@gmail.com>
2021-01-26 21:07:12 +0530

    Class 2

--- Class_1/template.cpp
+++ Class_1/template.cpp
@@ -1,34 +1,34 @@
-#include <bits/stdc++.h>  //Includes all the files
+#include <bits/stdc++.h>
 
 #include <unordered_map>
 typedef long long ll;
 typedef unsigned long long ull;
-
+#define whatis(x) cout << #x << " = " << x << endl;
+#define whatis2(x, y) cout << #x << " = " << x << " and " << #y << " = " << y << endl;
 #define MOD 1000000007
-
 #define IOS                           \
     ios_base::sync_with_stdio(false); \
     cin.tie(NULL);
-/*Used to desyncronize cin and cout with printf and scanf*/
+#define testcase     \
+    long long testn; \
+    cin >> testn;    \
+    while (testn--)
+#define all(x) x.begin(), x.end(),
+#define pb push_back
+#define loop(i, n) for (ll i = 0; i < n; i++)
+#define loopn(i, n) for (ll i = 0; i <= n; i++)
+#define loop1(i, n) for (ll i = 1; i < n; i++)
+#define loop1n(i, n) for (ll i = 1; i <= n; i++)
 
 using namespace std;
 
-int main() {
-    //Time O(n)
-    int n = 10;
-    for (int i = 0; i < n; i++) {
-    }
-    //Time Complexity O(logn)
-    for (int i = 0; i < n; i++) {
-        n = n / 2;
-    }
-    //Time Complexity O(n^3)
-    for(int i = 0;i<n;i++){
-        for(int i = 0;i<n;i++){
-            for(int i = 0;i < n;i++){
-            
-            }
-        }
-    }
+void solve() {
 
+}
+int main() {
+    IOS;
+    testcase {
+        solve();
+    }
+    return 0;
 }
