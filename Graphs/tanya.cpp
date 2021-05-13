9e657ad13c92caa57e9dfa0f3b9552eb7217b0a5
Tanya933 <tanya.rajpal0400@gmail.com>
2021-05-13 17:38:00 +0530

    correction done

--- Graphs/tanya.cpp
+++ Graphs/tanya.cpp
@@ -11,34 +11,21 @@ void dfs(int par,int curr,int k){
     }
 
     for(int child : graph[curr]){
-            cout<<child<<" ";
+            // cout<<child<<" ";
             if(child == par) continue ;
-            // if (curr == k) {
-            //     cout << "present"
-            //          << "\n";
-            //     return;
-            // }
-            dfs(child,curr,k);
+            dfs(curr,child,k);
     }
 }
 
 int main(){
     int n;
     cin>>n;
-    // vector<int> graph[n];
     for(int i=0;i<n-1;i++){
         int u,v;
         cin>>u>>v;
         graph[u].push_back(v);
         graph[v].push_back(u);
     }
-    // for(int i=0;i<n-1;i++){
-    //     for(auto j : graph[i]){
-    //         cout<<j<<" ";
-    //     }
-    //     cout<<"\n";
-    // }
-
     int k ;
     cout<<"enter node to be searched: ";
     cin>>k;
